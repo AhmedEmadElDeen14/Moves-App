5ee3da9a3f1b356f8470d2b9b39c42fc2d2abb7c
AhmedEmadElDeen14 <ahmedemadeldeen0@gmail.com>
2024-03-31 13:39:48 +0200

    Update app Colors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -6,7 +6,7 @@ class Colors{
   static Color rectangleBackgroundColor=const Color(0xff282A28);
   static Color selectedIconColor = const Color(0xffFFBB3B);
   static Color unselectedIconColor = const Color(0xffC6C6C6);
-  static Color selectedBookmarkColor = Color(0xffF7B539);
-  static Color unselectedBookmarkColor = Color(0xff514F4F);
+  static Color selectedBookmarkColor = const Color(0xffF7B539);
+  static Color unselectedBookmarkColor = const Color(0xff514F4F);
 
 }
